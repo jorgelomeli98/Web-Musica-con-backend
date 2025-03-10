925d89ca669d1edffc75e1c20bc86c13dd0854e0
jorgelomeli98 <jorgelomeli98@hotmail.com>
2025-03-10 00:11:06 -0600

    Cambio de peticiones al servicio de render

--- frontend/script.js
+++ frontend/script.js
@@ -3,7 +3,7 @@ const YOUTUBE_SEARCH_URL = "https://www.youtube.com/results?search_query=";
 
 async function obtenerArtistaAleatorio() {
     try {
-        const response = await fetch('http://127.0.0.1:8000/artista-aleatorio'); //127.0.0.1:8000 para pruebas locales
+        const response = await fetch('https://web-musica-aleatoria.onrender.com/artista-aleatorio'); //127.0.0.1:8000 para pruebas locales
         const data = await response.json();
         
         if (data.artista) {
@@ -21,7 +21,7 @@ async function obtenerArtistaAleatorio() {
 
 async function obtenerCancionDeArtista(artista) {
     try {
-        const response = await fetch(`http://127.0.0.1:8000/canciones/${artista}`); //127.0.0.1:8000 para pruebas locales
+        const response = await fetch(`https://web-musica-aleatoria.onrender.com/canciones/${artista}`); //127.0.0.1:8000 para pruebas locales
         const data = await response.json();
 
         if (data.canciones && data.canciones.length > 0) {
